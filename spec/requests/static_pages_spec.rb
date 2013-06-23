432d7e61caafd9cbe900cf5d04035a627f768a5e
Gayatri Singh <gsin4987@uni.sydney.edu.au>
2013-06-23 23:29:51 +1000

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
     
     describe "Home page" do
        before { visit root_path }
-       let(:heading)    { 'Welcome to Fcuk' }
+       let(:heading)    { 'Welcome to AFTER FACEBOOK' }
         let(:page_title) { '' }
         
     it_should_behave_like "all static pages"
@@ -32,6 +32,34 @@ describe "Static pages" do
                 end
             end
         end
+        
+        describe "for signed-in users" do
+            let(:user) { FactoryGirl.create(:user) }
+            before do
+                FactoryGirl.create(:micropost, user: user, content: "Lorem")
+                FactoryGirl.create(:micropost, user: user, content: "Ipsum")
+                sign_in user
+                visit root_path
+            end
+            
+            it "should render the user's feed" do
+                user.feed.each do |item|
+                    page.should have_selector("li##{item.id}", text: item.content)
+                end
+            end
+            
+            describe "follower/following counts" do
+                let(:other_user) { FactoryGirl.create(:user) }
+                before do
+                    other_user.follow!(user)
+                    visit root_path
+                end
+                
+                it { should have_link("0 following", href: following_user_path(user)) }
+                it { should have_link("1 followers", href: followers_user_path(user)) }
+            end
+        end
+    
     end
 
     describe "Help page" do
@@ -68,7 +96,7 @@ describe "Static pages" do
         click_link "Home"
         click_link "Sign up now!"
         page.should have_selector 'title', text: full_title('Sign up')
-        click_link "FCUK"
+        click_link "AFTER FACEBOOK"
         page.should have_selector 'title', text: full_title('')
     end
 end
